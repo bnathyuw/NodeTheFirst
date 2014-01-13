8b4ce710a0f03a099f6422ca0e86ad9fb389cdc5
matthew butt <matthew.butt@7digital.com>
2014-01-13 17:21:16 +0000

    ## requestHandler tests

---
+++ NodeTheFirst.Unit.Tests/tests/requestHandler.tests.js
@@ -0,0 +1,26 @@
+﻿var requestHandler = require("../../NodeTheFirst/requestHandler.js");
+
+exports.shouldRouteTheRequest = function(test) {
+	test.expect(2);
+	var actualPathname;
+	var actualResponse;
+	var pathname = "def";
+	var request = { url: "abc" };
+	var response = { b: 2 };
+
+	var parse = function(url) {
+		if (url === "abc") return { pathname: pathname };
+		return "";
+	};
+	
+	var route = function(p, r) {
+		actualPathname = p;
+		actualResponse = r;
+	};
+
+	requestHandler(parse, route).onRequest(request, response);
+
+	test.equal(actualPathname, pathname, "pathname");
+	test.equal(actualResponse, response, "response");
+	test.done();
+}
