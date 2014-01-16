bb013b093e5ac7292f38a3705daceba8cf117d05
matthew butt <matthew.butt@7digital.com>
2014-01-16 11:03:01 +0000

    ## Wire up status endpoint

--- NodeTheFirst/index.js
+++ NodeTheFirst/index.js
@@ -1,7 +1,9 @@
 ﻿var http = require("http");
 var url = require("url");
 var responseWriter = require("./responseWriter")();
-var router = require("./router")(responseWriter.writeResponse);
+var statusEndpoint = require("./statusEndpoint")();
+var routes = [statusEndpoint];
+var router = require("./router")(responseWriter.writeResponse, routes);
 var requestHandler = require("./requestHandler")(url.parse, router.route);
 var server = require("./server")(http, 1337, requestHandler.handleRequest);
 
