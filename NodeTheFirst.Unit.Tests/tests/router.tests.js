b9a97db0a4d9214f048dc634dc776497c901eec5
matthew butt <matthew.butt@7digital.com>
2014-01-16 10:31:46 +0000

    Added basic routing lookup

--- NodeTheFirst.Unit.Tests/tests/router.tests.js
+++ NodeTheFirst.Unit.Tests/tests/router.tests.js
@@ -1,51 +1,93 @@
 ﻿var Router = require("../../NodeTheFirst/router.js");
 var nodeunit = require("nodeunit");
 
-var status, actualResponse, content, suppliedResponse;
+(function() {
+	var status, actualResponse, content, suppliedResponse;
 
-exports["route"] = nodeunit.testCase({
-	setUp: function(callback) {
-		status = null;
-		actualResponse = null;
-		content = null;
-		suppliedResponse = { a: 1 };
+	exports["when I call an unconfigured route"] = nodeunit.testCase({
+		setUp: function(callback) {
+			status = null;
+			actualResponse = null;
+			content = null;
+			suppliedResponse = { a: 1 };
 
-		var router = Router(function(r, s, c) {
-			status = s;
-			actualResponse = r;
-			content = c;
-		});
+			var router = Router(function(r, s, c) {
+				status = s;
+				actualResponse = r;
+				content = c;
+			});
 
-		router.route("/", suppliedResponse);
+			router.route("/", suppliedResponse);
 
-		callback();
-	},
+			callback();
+		},
 
-	tearDown: function(callback) {
-		callback();
-	},
+		tearDown: function(callback) {
+			callback();
+		},
 
-	"should write status": function(test) {
-		test.expect(1);
+		"should write status": function(test) {
+			test.expect(1);
 
-		test.equal(status, 200, "Status");
+			test.equal(status, 200, "Status");
 
-		test.done();
-	},
+			test.done();
+		},
 
-	"should pass through response for modification": function(test) {
-		test.expect(1);
+		"should pass through response for modification": function(test) {
+			test.expect(1);
 
-		test.equal(actualResponse, suppliedResponse, "Content");
+			test.equal(actualResponse, suppliedResponse, "Content");
 
-		test.done();
-	},
+			test.done();
+		},
 
-	"should write output": function(test) {
-		test.expect(1);
+		"should write output": function(test) {
+			test.expect(1);
 
-		test.equal(content, "Hello, world!", "Content");
+			test.equal(content, "Hello, world!", "Content");
 
-		test.done();
-	}
-});
+			test.done();
+		}
+	});
+})();
+
+(function() {
+	var status, actualResponse, content, suppliedResponse;
+	
+	exports["when I call a configured route"] = nodeunit.testCase({
+		setUp: function(callback) {
+			status = null;
+			actualResponse = null;
+			content = null;
+			suppliedResponse = { a: 1 };
+
+			var configuredEndpoint = function(response, writeResponse) {
+				writeResponse(response, 123, "Hi there", "text/gobbledigook");
+			};
+
+			var routes = [{ path: "/here-i-am", handle: configuredEndpoint }];
+
+			var router = Router(function(r, s, c) {
+				status = s;
+				actualResponse = r;
+				content = c;
+			}, routes);
+			router.route("/here-i-am", suppliedResponse);
+
+			callback();
+		},
+
+		tearDown: function(callback) {
+			callback();
+		},
+
+		"should write status": function(test) {
+			test.expect(1);
+
+			test.equal(status, 123, "Status");
+
+			test.done();
+		}
+	});
+})();
--- NodeTheFirst/router.js
+++ NodeTheFirst/router.js
@@ -1,6 +1,18 @@
-﻿module.exports = function(writeResponse) {
+﻿module.exports = function(writeResponse, routes) {
+	routes = routes || [];
 
 	function route(pathname, response) {
+		console.log(routes);
+		
+        for (var i = 0; i < routes.length; i++) {
+	        var r = routes[i];
+	        console.log(r);
+	        if (r.path == pathname) {
+		        r.handle(response, writeResponse);
+		        return;
+	        }
+        }
+        
 		writeResponse(response, 200, "Hello, world!", "text/plain");
 	}
 
